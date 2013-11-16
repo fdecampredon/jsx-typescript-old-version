a2b5a48887c2600f678abe3f6ee973aa5c73fa08
Cyrus Najmabadi <cyrusn@microsoft.com>
2013-11-16 00:45:32 -0800

    Update baseline after merge.

--- tests/baselines/reference/objectTypesWithPredefinedTypesAsName2.js
+++ tests/baselines/reference/objectTypesWithPredefinedTypesAsName2.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-void {}; // parse error unlike the others
-=======
 // it is an error to use a predefined type as a type name
-void {} // parse error unlike the others
-;
->>>>>>> develop
+void {}; // parse error unlike the others
