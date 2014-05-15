5ca6425f4a501521c440301ba5909860a6be53cb
Dick van den Brink <d_vandenbrink@outlook.com>
2014-05-15 22:39:39 +0200

    Moved FormattingOptions class into TypeScript module

--- src/compiler/syntax/formattingOptions.ts
+++ src/compiler/syntax/formattingOptions.ts
@@ -1,11 +1,13 @@
 ///<reference path='references.ts' />
 
-class FormattingOptions {
-    constructor(public useTabs: boolean,
-                public spacesPerTab: number,
-                public indentSpaces: number,
-                public newLineCharacter: string) {
-    }
+module TypeScript {
+    export class FormattingOptions {
+        constructor(public useTabs: boolean,
+            public spacesPerTab: number,
+            public indentSpaces: number,
+            public newLineCharacter: string) {
+        }
 
-    public static defaultOptions = new FormattingOptions(/*useTabs:*/ false, /*spacesPerTab:*/ 4, /*indentSpaces:*/ 4, /*newLineCharacter*/ "\r\n");
+        public static defaultOptions = new FormattingOptions(/*useTabs:*/ false, /*spacesPerTab:*/ 4, /*indentSpaces:*/ 4, /*newLineCharacter*/ "\r\n");
+    }
 }
